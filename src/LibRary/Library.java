31987e93cb7738236d25eb271e536758d5e52c40
ClanEZ <bernardosoto30rus@gmail.com>
2025-01-02 02:18:13 +0400

    init

---
+++ src/LibRary/Library.java
@@ -0,0 +1,77 @@
+package LibRary;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Library {
+    private Map<Integer, Book> books;
+
+    public Library() {
+        this.books = new HashMap<>();
+    }
+
+    public void addBook(Book book) {
+        books.put(book.getId(), book);
+    }
+
+    public Book getBookById(int id) {
+        return books.get(id);
+    }
+
+    public void displayBooks() {
+        for (Book book : books.values()) {
+            System.out.println(book);
+        }
+    }
+
+
+    public List<Book> getBooks() {
+        return books.values().stream()
+                .sorted(Comparator.comparing(Book::getTitle))
+                .collect(Collectors.toList());
+    }
+
+    public List<Book> getBooks(boolean isAvailable) {
+        return books.values().stream()
+                .filter(book -> book.isAvailable() == isAvailable)
+                .sorted(Comparator.comparing(Book::getTitle))
+                .collect(Collectors.toList());
+    }
+
+
+    public long getBookCount() {
+        return books.size();
+    }
+
+
+    public long getBookCount(boolean isAvailable) {
+        return books.values().stream()
+                .filter(book -> book.isAvailable() == isAvailable)
+                .count();
+    }
+
+
+    public List<String> getAuthors() {
+        return books.values().stream()
+                .map(Book::getAuthor)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+
+    public List<Book> getBooks(Comparator<Book> comparator) {
+        return books.values().stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
+
+    public List<Book> getBooks(boolean isAvailable, Comparator<Book> comparator) {
+        return books.values().stream()
+                .filter(book -> book.isAvailable() == isAvailable)
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/LibRary/Main.java
@@ -0,0 +1,43 @@
+package LibRary;
+
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Library lib = new Library();
+
+        lib.addBook(new Book(1,"DubZeleniy","Pushkin",true));
+        lib.addBook(new Book(2,"War and Peace","Tolstoy",false));
+        lib.addBook(new Book(3,"Java","JohnDoe",false));
+        lib.addBook(new Book(4,"Revizor","Gogol",true));
+
+
+
+        System.out.println("All books sorted by title:");
+        lib.getBooks().forEach(System.out::println);
+
+
+        System.out.println("\nВсе доступные книги:");
+        lib.getBooks(true).forEach(System.out::println);
+
+
+        System.out.println("\nОбщее количество книг " + lib.getBookCount());
+
+
+        System.out.println("\nКоличество доступных книг по доступу " + lib.getBookCount(true));
+
+
+        System.out.println("\nСписок книг по авторам, без дубликатов");
+        lib.getAuthors().forEach(System.out::println);
+
+
+        System.out.println("\nУниверсальный метод, сортировка по автору");
+        lib.getBooks(Comparator.comparing(Book::getAuthor)).forEach(System.out::println);
+
+
+        System.out.println("\nДоступные книги по автору: ");
+        lib.getBooks(true, Comparator.comparing(Book::getAuthor)).forEach(System.out::println);
+    }
+}
+
